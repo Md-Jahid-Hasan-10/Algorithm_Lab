dad3892cea2c7b9607318a2520d7ea243f030247
Mohammad Jahid Hasan <122695080+Md-Jahid-Hasan-10@users.noreply.github.com>
2025-04-02 22:15:32 +0600

    Insertion_sort.cpp

--- Sorting/Insertion_sort.cpp
+++ Sorting/Insertion_sort.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter value of n \n";
     cin >> n;
     int ar[n];
+    cout << "Enter n element \n";
     for(int i = 0; i < n; i++){
         cin >> ar[i];
     }
@@ -19,4 +21,9 @@ int main()
         }
         ar[prev + 1] = key;
     }
+    cout << "After sort array is = ";
+    for(int i = 0; i < n; i++){
+        cout << ar[i] << " ";
+    }
+    cout << endl;
 }
