8abecb525346b0abd63e659c7dd4814ada77ecc2
Mohammad Jahid Hasan <122695080+Md-Jahid-Hasan-10@users.noreply.github.com>
2025-04-02 22:17:08 +0600

    heap_sort.cpp

--- Sorting/heap_sort.cpp
+++ Sorting/heap_sort.cpp
@@ -6,16 +6,16 @@ void max_Heapify(int ar[],int node,int n)
     int largest = node;
     int left = 2 * node + 1;
     int right = 2 * node + 2;
-    //cout << node <<" <- node " ;
+    cout << node <<" <- node " ;
     if(left < n && ar[left] > ar[largest]){
-        //cout << left <<" left " ;
+        cout << left <<" left " ;
         largest = left;
     }
     if(right < n && ar[right] > ar[largest]){
         largest = right;
-        //cout << right <<" right " ;
+        cout << right <<" right " ;
     }
-    //cout << largest <<" <- largest "<< endl;
+    cout << largest <<" <- largest "<< endl;
     if(largest != node){
         swap(ar[largest],ar[node]);
         max_Heapify(ar,largest,n);
@@ -43,6 +43,7 @@ int main()
         cin >> ar[i];
     }
     build_max_heap(ar,n);
+    cout << "After sort array is = ";
     for(int i = 0; i < n; i++){
         cout << ar[i] << " ";
     }
