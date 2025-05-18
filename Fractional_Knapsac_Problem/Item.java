40a8b820ab61d84e09d4e9171474b94d3bd712a1
Mohammad Jahid Hasan <122695080+Md-Jahid-Hasan-10@users.noreply.github.com>
2025-05-18 12:16:12 +0600

    Create Item.java

---
+++ Fractional_Knapsac_Problem/Item.java
@@ -0,0 +1,26 @@
+package F.K.P;
+
+public class Item {
+	private double weight;
+	private double value;
+	private int item_num;
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public int getItem_num() {
+		return item_num;
+	}
+	public void setItem_num(int item_num) {
+		this.item_num = item_num;
+	}
+	
+}
